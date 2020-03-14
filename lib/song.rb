a787adfbb1af03a5e13b7a192a2b31be84db42b9
Christopher Chaison <cchaison0@gmail.com>
2020-03-14 16:31:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,6 +18,23 @@ class Song
     song = Song.new_by_name(name)
   end
 
+  def self.new_from_filename(fn)
+    song_and_artist = filename_helper(fn)
+    artist = song_and_artist[0]
+    song_name = song_and_artist[1]
+    song = Song.create_by_name(song_name)
+    song.artist_name = artist
+  end
+
+  def filename_helper(fn)
+    s_a_array = []
+    song_and_artist = fn.split(" - ")
+    song_name_previous = song_and_artist[1]
+    song_array = song_name_previous.split(".")
+    s_a_array << song_and_artist[0]
+    s_a_array << song_array[0]
+  end
+  
   def self.find_by_name(name)
     self.all.find{|s| s.name == name}
   end
